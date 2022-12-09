84540b0e4fa15d3b1f75f7820bc7b943273ac02b
Dwight-Reed <reeddwight1alt@gmail.com>
2022-12-09 02:33:44 -0500

    fix urls didn't realize not having a trailing backslash could cause issues

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -8,23 +8,23 @@ from .forms import LoginForm
 urlpatterns = [
     # Normal pages
     path("", views.index, name="index"),
-    path("wiki/<str:title>/edit", views.edit, name="edit"),
-    path("wiki/<str:title>", views.wiki, name="wiki"),
-    path("search", views.search_results, name="search_results"),
-    path("image/<str:name>", views.image, name="image"),
+    path("wiki/<str:title>/edit/", views.edit, name="edit"),
+    path("wiki/<str:title>/", views.wiki, name="wiki"),
+    path("search/", views.search_results, name="search_results"),
+    path("image/<str:name>/", views.image, name="image"),
 
     # Create pages
-    path("new_page", views.EntryCreateView.as_view(), name="new_page"),
-    path("new_image", views.ImageCreateView.as_view(), name="new_image"),
+    path("new_page/", views.EntryCreateView.as_view(), name="new_page"),
+    path("new_image/", views.ImageCreateView.as_view(), name="new_image"),
 
     # Auth
-    path("login", auth_views.LoginView.as_view(authentication_form=LoginForm, redirect_field_name=views.index), name="login"),
-    path("logout", views.logout_view, name="logout"),
-    path("register", views.register, name="register"),
+    path("login/", auth_views.LoginView.as_view(authentication_form=LoginForm, redirect_field_name=views.index), name="login"),
+    path("logout/", views.logout_view, name="logout"),
+    path("register/", views.register, name="register"),
 
     # Other
-    path("wiki/random", views.random_page, name="random"),
+    path("wiki/random/", views.random_page, name="random"),
 
     # API
-    path("api/search", views.search, name="search"),
+    path("api/search/", views.search, name="search"),
 ]
