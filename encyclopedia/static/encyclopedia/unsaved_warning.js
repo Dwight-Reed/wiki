eb971fee3a6ece545b51f47eb8bc86bc41f8a08c
Dwight-Reed <reeddwight1alt@gmail.com>
2022-12-09 20:54:36 -0500

    fix submit button on new_page and edit warning user about unsaved changes

--- encyclopedia/static/encyclopedia/unsaved_warning.js
+++ encyclopedia/static/encyclopedia/unsaved_warning.js
@@ -1,13 +1,22 @@
 let initialContent
+let disabled = false
+// Disable warning if submitting a valid form
+function onSubmitClicked() {
+  if (document.querySelector('.entry-form').checkValidity()) {
+    disabled = true;
+  }
+}
 
 addEventListener('DOMContentLoaded', () => {
-  // Save original content to compare to
+  // Save original content to compare to later
   initialContent = document.querySelector('#id_content').value
+  // If the server returned an error, assume content is changed as the initial value can no longer be retrieved
+  submitFailed = document.querySelector('.errorlist.nonfield')
 })
 
 window.addEventListener('beforeunload', function (event) {
   // Warn user before leaving page if any changes were made to the content (edit or new page)
-  if (content = document.querySelector('#id_content').value != initialContent) {
+  if ((document.querySelector('#id_content').value != initialContent || submitFailed) && !disabled) {
     event.preventDefault();
   }
 });
