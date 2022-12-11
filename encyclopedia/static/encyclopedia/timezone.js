ccfafa15269a25f52b1543a43a9a04b891f3f43a
Dwight-Reed <reeddwight1alt@gmail.com>
2022-12-11 03:57:10 -0500

    added timezone.js - converts tags with 'date' class to local timezone

---
+++ encyclopedia/static/encyclopedia/timezone.js
@@ -0,0 +1,6 @@
+addEventListener('DOMContentLoaded', () => {
+  document.querySelectorAll(".date").forEach((element) => {
+    let date = new Date(element.innerHTML)
+    element.innerHTML = date.toLocaleString()
+  })
+})
